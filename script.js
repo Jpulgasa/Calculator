e5c5ba19bd6f08a22a9f70028d354bbaa7035ec4
Juan Pulgarin <juanpulgarin.saldarriaga@gmail.com>
2025-12-11 13:39:46 -0500

    The calculator now calculates
    Updated and completed the logic to populate the
    display and last thing done was add the logic to
    operate.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let numberOne = 0;
-let numberTwo = 0;
-let operator = null;
-let displayContent = null;
-
 const display = document.getElementById('calculator-display')
 const calculatorBtns = document.querySelectorAll('.calculator-btns')
+const operatorBtns = document.querySelectorAll('.calculator-operator-btns')
+
+let numberOne = 0;
+let numberTwo = 0;
+let operator = "";
+let displayContent = "";
 
 function add (numA, numB) {
     const result = numA + numB;
@@ -31,31 +32,47 @@ function divide (numA, numB) {
 }
 
 function operate (operator, numberOne, numberTwo) {
-    if (operator === "add") {
+    if (operator === "+") {
         return add(numberOne, numberTwo);
-    } else if (operator === "subtract") {
+    } else if (operator === "-") {
         return subtract(numberOne, numberTwo);
-    } else if (operator === "multiply") {
+    } else if (operator === "X") {
         return multiply(numberOne, numberTwo);
-    } else if (operator === "divide") {
+    } else if (operator === "÷") {
         return divide(numberOne, numberTwo);
     }
 }
 
-// function populateDisplay () {
-//     display.textContent = "holaaa"
-//     console.log("yes it was pressed");
-// }
-
-// calculatorBtns.addEventListener("click", populateDisplay())
-
-
 calculatorBtns.forEach((button) => {
     button.addEventListener("click", () => {
     display.textContent += button.textContent;
-    displayContent = button.textContent;
-    console.log(displayContent);
-    displayContent + 1;
-    console.log("Now the number is " + displayContent)
+    displayContent += button.textContent;
+    /* numberOne = displayContent;
+    displayContent = "";*/
+    //console.log(displayContent); 
     })
 });
+
+operatorBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.textContent !== "=" && button.textContent !== "C") {
+            numberOne = parseFloat(displayContent);
+            operator = button.textContent;
+            display.textContent = "";
+            displayContent = "";
+        } else if (button.textContent === "=") {
+            numberTwo = parseFloat(displayContent);
+            const result = operate(operator, numberOne, numberTwo);
+            display.textContent = result;
+            displayContent = result;
+        } else {
+            numberOne = 0;
+            numberTwo = 0;
+            operator = "";
+            displayContent = "";
+            display.textContent = "";
+        }
+    })
+})
+
+
